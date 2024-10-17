991ea9ab2c6f06c7615bbcc9f069716a9981a2a3
tl-genie <genie@twelvelabs.io>
2024-10-18 01:11:51 +0900

    feat: example

--- examples/embed.py
+++ examples/embed.py
@@ -19,52 +19,50 @@ with TwelveLabs(API_KEY) as client:
         if task.metadata is not None:
             print(f"  metadata: {task.metadata}")
 
-    def print_video_embeddings(id: str):
-        task = client.embed.task.retrieve(id)
-        if task.video_embeddings is not None:
-            for v in task.video_embeddings:
-                print(
-                    f"embedding_scope={v.embedding_scope} start_offset_sec={v.start_offset_sec} end_offset_sec={v.end_offset_sec}"
-                )
-                print(f"embeddings: {", ".join(str(v.embedding.values))}")
-
     def on_task_update(task: EmbeddingsTask):
         print(f"  Status={task.status}")
 
     engine_name = "Marengo-retrieval-2.6"
 
-    text_embedding = client.embed.create(
+    res = client.embed.create(
         engine_name=engine_name,
         text="man walking across the street",
         text_truncate="start",
     )
-    print(f"Created text embedding: engine_name={text_embedding.engine_name}")
+    print(f"Created text embedding: engine_name={res.engine_name}")
+    if res.text_embedding is not None:
+        print(f"  embeddings: {", ".join(str(res.text_embedding.values))}")
+
+    res = client.embed.create(
+        engine_name=engine_name,
+        image_file=os.path.join(os.path.dirname(__file__), "assets/search_sample.png"),
+    )
+    print(f"Created image embedding: engine_name={res.engine_name}")
+    if res.image_embedding is not None:
+        print(f"  embeddings: {", ".join(str(res.image_embedding.values))}")
+
+    res = client.embed.create(
+        engine_name=engine_name,
+        audio_file=os.path.join(os.path.dirname(__file__), "assets/audio_sample.png"),
+    )
+    print(f"Created audio embedding: engine_name={res.engine_name}")
+    if res.audio_embedding.segments is not None:
+        for segment in res.audio_embedding.segments:
+            print(
+                f"  embedding_scope={segment.embedding_scope} start_offset_sec={segment.start_offset_sec} end_offset_sec={segment.end_offset_sec}"
+            )
+            print(f"  embeddings: {", ".join(str(segment.values))}")
 
     video_path = os.path.join(os.path.dirname(__file__), "assets/example.mp4")
     task = client.embed.task.create(engine_name=engine_name, video_file=video_path)
     print(
-        f"Created task of text embedding: id={task.id} engine_name={task.engine_name} status={task.status}"
+        f"Created task: id={task.id} engine_name={task.engine_name} status={task.status}"
     )
 
     status = task.wait_for_done(callback=on_task_update)
     print(f"Embedding done: {status}")
-    print_video_embeddings(task.id)
 
-    image_embedding = client.embed.create(
-        engine_name=engine_name,
-        image_file=os.path.join(os.path.dirname(__file__), "assets/search_sample.png"),
-    )
-    print(f"Created image embedding: engine_name={image_embedding.engine_name}")
-
-    task = client.embed.task.create(engine_name=engine_name, video_file=video_path)
-    print(
-        f"Created task of image embedding: id={task.id} engine_name={task.engine_name} status={task.status}"
-    )
-
-    status = task.wait_for_done(callback=on_task_update)
-    print(f"Embedding done: {status}")
-    
-    task = client.embed.task.retrieve(task.id)
+    task = task.retrieve()
     if task.video_embeddings is not None:
         for v in task.video_embeddings:
             print(
